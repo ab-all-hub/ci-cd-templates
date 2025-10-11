41ccf224ab0a63d10bc019e06535cb6670cb31b3
Anand Babu P <151912807+anbu143dude@users.noreply.github.com>
2025-10-11 12:39:27 +0530

    ab - sync

--- dev/main.tf
+++ dev/main.tf
@@ -1,18 +1,18 @@
 module "rg" {
-    source      = "git::https://github.com/owner/terraform-modules.git//azure/resource_group?ref=main"
+    source      = "git::https://github.com/ab-all-hub/terraform-modules.git//azure/resource_group?ref=main"
     name        = var.rg_name
     location    = var.rg_location
 }
 
 module "vnet" {
-    source              = "git::https://github.com/owner/terraform-modules.git//azure/virtual_network?ref=main"
+    source              = "git::https://github.com/ab-all-hub/terraform-modules.git//azure/virtual_network?ref=main"
     location            = module.rg.rg_location
     resource_group_name = module.rg.rg_name
     virtual_network     = var.virtual_network
 }
 
 module "lb" {
-    source              = "git::https://github.com/owner/terraform-modules.git//azure/load_balancer_private?ref=main"
+    source              = "git::https://github.com/ab-all-hub/terraform-modules.git//azure/load_balancer_private?ref=main"
     location            = module.rg.rg_location
     resource_group_name = module.rg.rg_name
     name                = var.lb_name
@@ -26,7 +26,7 @@ module "lb" {
 }
 
 module "vm" {
-    source              = "git::https://github.com/owner/terraform-modules.git//azure/virtual_machine?ref=main"
+    source              = "git::https://github.com/ab-all-hub/terraform-modules.git//azure/virtual_machine?ref=main"
     location            = module.rg.rg_location
     resource_group_name = module.rg.rg_name
     subnet_id           = module.vnet.snet_id[0]
